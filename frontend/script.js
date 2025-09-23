3016310a1363e30b79eebf8a5ffa9c28b840c014
Kafia Kauser <kkauser50170@gmail.com>
2025-09-23 20:45:36 +0530

    Add files via upload

---
+++ frontend/script.js
@@ -0,0 +1,13 @@
+document.getElementById("ngoForm").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const desc = document.getElementById("desc").value;
+
+  const response = await fetch("http://127.0.0.1:8000/match", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ description: desc }),
+  });
+
+  const data = await response.json();
+  document.getElementById("output").innerText = data.recommendation;
+});
